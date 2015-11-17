5b51142681165fe019bac933d6427809277a2868
Sean Chang <schang732@gmail.com>
2015-11-17 14:35:20 -0700

    first commit

---
+++ loltilt/NetworkController.swift
@@ -0,0 +1,12 @@
+//
+//  NetworkController.swift
+//  loltilt
+//
+//  Created by Sean Chang on 11/16/15.
+//  Copyright © 2015 Sean Chang. All rights reserved.
+//
+
+import Foundation
+
+//key 21f813d8-e04d-4065-8f3a-c7b595fd21a5
+//https://na.api.pvp.net/api/lol/na/v1.4/summoner/by-name/player?api_key=21f813d8-e04d-4065-8f3a-c7b595fd21a5
---
+++ loltilt/Player.swift
@@ -0,0 +1,13 @@
+//
+//  Player.swift
+//  loltilt
+//
+//  Created by Sean Chang on 11/16/15.
+//  Copyright © 2015 Sean Chang. All rights reserved.
+//
+
+import Foundation
+
+class Player {
+    
+}
