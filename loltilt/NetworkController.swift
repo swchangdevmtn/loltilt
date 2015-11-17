11b48da486ef020ff5dc4c63225a7bd11a15a156
Sean Chang <schang732@gmail.com>
2015-11-17 16:08:11 -0700

    NetworkController

--- loltilt/NetworkController.swift
+++ loltilt/NetworkController.swift
@@ -8,5 +8,63 @@
 
 import Foundation
 
-//key 21f813d8-e04d-4065-8f3a-c7b595fd21a5
-//https://na.api.pvp.net/api/lol/na/v1.4/summoner/by-name/player?api_key=21f813d8-e04d-4065-8f3a-c7b595fd21a5
+class NetworkController {
+    static let ApiKey = "21f813d8-e04d-4065-8f3a-c7b595fd21a5"
+    static func searchURL(region: String, searchTerm: String) -> NSURL {
+        let modifiedSearchTerm = searchTerm.removeWhitespace().lowercaseString
+        return NSURL(string: "https://\(region).api.pvp.net/api/lol/\(region)/v1.4/summoner/by-name/\(modifiedSearchTerm)?api_key=\(ApiKey)")!
+    }
+    
+    static func currentGame(region: String, summonerId: String) -> NSURL {
+        var platformId = ""
+        if region == "na" {
+            platformId = "NA1"
+        }
+        if region == "br" {
+            platformId = "BR1"
+        }
+        if region == "lan" {
+            platformId = "LA1"
+        }
+        if region == "las" {
+            platformId = "LA2"
+        }
+        if region == "oce" {
+            platformId = "OC1"
+        }
+        if region == "eune" {
+            platformId = "EUN1"
+        }
+        if region == "tr" {
+            platformId = "TR1"
+        }
+        if region == "ru" {
+            platformId = "RU"
+        }
+        if region == "euw" {
+            platformId = "EUW1"
+        }
+        if region == "kr" {
+            platformId = "KR"
+        }
+        return NSURL(string: "https://\(region).api.pvp.net/observer-mode/rest/consumer/getSpectatorGameInfo/\(platformId)/\(summonerId)?api_key=\(ApiKey)")!
+    }
+    
+    static func pastTenGames(region: String, summonerId: String) -> NSURL {
+        return NSURL(string: "https://\(region).api.pvp.net/api/lol/\(region)/v1.3/game/by-summoner/\(summonerId)/recent?api_key=\(ApiKey)")!
+    }
+    
+    static func summonerStats(region: String, summonerId: String) -> NSURL {
+        return NSURL(string: "https://\(region).api.pvp.net/api/lol/\(region)/v1.3/stats/by-summoner/\(summonerId)/ranked?&api_key=\(ApiKey)")!
+    }
+}
+
+extension String {
+    func replace(string:String, replacement:String) -> String {
+        return self.stringByReplacingOccurrencesOfString(string, withString: replacement, options: NSStringCompareOptions.LiteralSearch, range: nil)
+    }
+    
+    func removeWhitespace() -> String {
+        return self.replace(" ", replacement: "")
+    }
+}
